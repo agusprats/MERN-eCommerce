1fe1bb89034664a18741b9c133ded01dad4676db
aguspop <59830072+aguspop@users.noreply.github.com>
2020-09-24 18:15:00 -0300

    animation and modal

--- src/App.js
+++ src/App.js
@@ -1,125 +1,125 @@
 import React from 'react';
-import data from "./data.json";
+import data from './data.json';
 import Products from "./components/Products";
-import Filter from './components/Filter';
-import Cart from './components/Cart';
-    
+import Filter from "./components/Filter";
+import Cart from "./components/Cart";
+
 
 class App extends React.Component {
-  
-    constructor(){
-      super();
-      this.state = {
-        products: data.products,
-        cartItems: localStorage.getItem("cartItems")
-        ? JSON.parse(localStorage.getItem("cartItems"))
-        : [],
-        size:"",
-        sort: "",
-      };
-    }
+  constructor(){
+    super();
+    this.state={
+      products: data.products,
+      cartItems:localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      :[], 
+      size: "",
+      sort: "",
+    };
+  }
+  createOrder=(order)=>{
+    alert("Need to save order for" + order.name)
+  }
 
-createOrder = (order) => {
-  alert("Need to save the order")
-}
-
-
-    removeFromCart = (product) => {
-      const cartItems = this.state.cartItems.slice();
-      this.setState({cartItems: cartItems.filter(x=>x._id !== product._id),
-      });
-      localStorage.setItem(
-      "cartItems", 
-      JSON.stringify(cartItems.filter((x) =>x._id !== product._id))
+  removeFromCart = (product)=>{
+    const cartItems=this.state.cartItems.slice();
+    this.setState({
+      cartItems:cartItems.filter((x)=>x._id !== product._id),
+    });
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(cartItems.filter((x)=>x._id !== product._id))
       );
-    };
-    addToCart=(product) => {
-      const cartItems = this.state.cartItems.slice();
-      let alreadyInCart = false;
-      cartItems.forEach(item => {
-        if(item._id === product._id){
-          item.count++;
-          alreadyInCart = true;
-        }
-      });
-      if(!alreadyInCart){
-        cartItems.push({...product, count: 1});
+  };
+
+  addToCart=(product)=>{
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart=false;
+    cartItems.forEach(item => {
+      if(item._id === product._id){
+        item.count++;
+        alreadyInCart = true;
       }
-      this.setState({cartItems});
-      localStorage.setItem("cartItems", JSON.stringify(cartItems));
-    };
-    sortProducts = (event) => {
+    });
+    if(!alreadyInCart){
+      cartItems.push({...product, count:1});
+    }
+    this.setState({cartItems});
+    localStorage.setItem("cartItems",JSON.stringify(cartItems));
+  };
+  
+
+
+  sortProducts=(event)=>{
       //impl
-      const sort = event.target.value;
-      console.log(event.target.value);
-      this.setState((state) => ({
-          sort: sort,
-          products: this.state.products
-          .slice()
-          .sort((a,b) => 
-            sort === "lowest"
-            ? a.price > b.price 
-            ? 1
-            :-1
-            :sort === "highest"
-            ? a.price < b.price
-            ? 1
-            :-1
-            :a._id < b._id
-            ? 1
-            :-1
-          ),
+      const sort= event.target.value;
+      this.setState((state)=>({
+        sort: sort,
+        products:this.state.products
+        .slice()
+        .sort((a,b)=>
+          sort==="lowest"
+          ?a.price>b.price
+          ?1
+          :-1
+          :sort==="highest"
+          ?a.price<b.price
+          ? 1
+          :-1
+          :a._id<b._id
+          ? 1
+          :-1
+        ), 
       }));
-    };
-    filterProducts = (event) => {
-      //Impl
-      console.log(event.target.value);
-      if(event.target.value === ""){
-        this.setState({size: event.target.value, products:data.products})
-      }else {
+  };
+  filterProducts=(event)=>{
+      //impl
+      if(event.target.value===""){
+        this.setState({size:event.target.value, products:data.products});
+      }else{
         this.setState({
           size: event.target.value,
-          products: data.products.filter(product => product.availableSizes.indexOf(event.target.value)>=0),
+          products: data.products.filter(product=>product.availableSizes.indexOf(event.target.value)>=0),
         });
       }
-     
-    };
+  };
 
-
-
-    render() {
+  render(){
     return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Dress Lovers</a>
-      </header>
-      <main>
-      <div className="content">
-        <div className="main">
-          <Filter count={this.state.products.length}
-            size={this.state.size}
-            sort={this.state.sort}
-            filterProducts={this.filterProducts}
-            sortProducts={this.sortProducts}
-            >
-
-          </Filter>
-            <Products products={this.state.products} addToCart={this.addToCart}></Products>
-        </div>
-        <div className="sidebar">
-            <Cart cartItems={this.state.cartItems} 
-            removeFromCart={this.removeFromCart}
-            createOrder={this.createOrder}
-            />
-        </div>
+      <div className="grid-container">
+          <header>
+            <a href="/">React Shopping Cart</a>
+          </header>
+          <main>
+            <div className="content">
+              <div className="main">
+                <Filter count={this.state.products.length}
+                         size={this.state.size}
+                         sort={this.state.sort}
+                         filterProducts={this.filterProducts}
+                         sortProducts={this.sortProducts} 
+                ></Filter>
+                <Products products={this.state.products} 
+                addToCart={this.addToCart}></Products>
+                </div>
+              <div className="sidebar">
+                <Cart 
+                  cartItems={this.state.cartItems} 
+                  removeFromCart={this.removeFromCart} 
+                  createOrder={this.createOrder}
+                  />
+                  </div>
+            </div>
+          </main>
+          <footer>
+            All rights reserved
+          </footer>
       </div>
-      </main>
-      <footer>
-        All rights reserved
-      </footer>
-    </div>
-  );
-}
+    );
+  }
+  
+  
 }
 
 export default App;
+
--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
 export default function formatCurrency(num){
-return "$" + Number(num.toFixed(1)).toLocaleString() + " ";
+return "$" + Number(num).toLocaleString() + " ";
 
 }
